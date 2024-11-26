14077501f423bbe2a257f30087291df095c53292
alswn1 <alswn5790@naver.com>
2024-11-26 13:37:46 +0900

    정리

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,6 @@ function Header() {
     return (
         <div className='nav'>
             <h1>Schedule My Day</h1>
-            <div className='login'>
-                <div>login</div>
-                <div>Signin</div>
-            </div>
         </div>
     )
 };
